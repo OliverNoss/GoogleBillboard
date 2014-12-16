5d1a13a0d5871178643ea81820463ec7a93c770f
OliverNoss <odnoss@sbcglobal.net>
2014-12-16 13:14:45 -0800

    fixed something

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,14 +1,45 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+
 public void setup()  
 {            
-     noLoop();  
+	noLoop();  
 }  
 public void draw()  
 {   
+	System.out.println(theAnswer());
 	//not needed for this assignment
 }  
-public boolean isPrime(double dNum)  
-{   
-    //to be finished later   
-    return true;  
-} 
+public boolean isPrime(double num)
+{
+	boolean temp=true;
+	if(num>1)
+	{
+		for(int i=2; i<=Math.sqrt(num);i++)
+		{
+			if(num%i==0&&i!=num)
+				temp=false;
+
+		}
+	}
+
+	else{
+		temp = false;
+	}
+
+	return temp;
+}
+public double theAnswer()
+{
+	String digits;
+	double dNum;
+	double dNumFinal=0;
+	for (int n = 2; n<e.length()-10; n++)
+	{
+		digits = e.substring(n,n+10);
+		dNum = Double.parseDouble(digits); 
+		if (isPrime(dNum))
+			dNumFinal=dNum;
+		
+	}
+	return dNumFinal;
+}
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -15,19 +15,50 @@ import java.io.IOException;
 public class GoogleBillboard extends PApplet {
 
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+
 public void setup()  
 {            
-     noLoop();  
+	noLoop();  
 }  
 public void draw()  
 {   
+	System.out.println(theAnswer());
 	//not needed for this assignment
 }  
-public boolean isPrime(double dNum)  
-{   
-    //to be finished later   
-    return true;  
-} 
+public boolean isPrime(double num)
+{
+	boolean temp=true;
+	if(num>1)
+	{
+		for(int i=2; i<=Math.sqrt(num);i++)
+		{
+			if(num%i==0&&i!=num)
+				temp=false;
+
+		}
+	}
+
+	else{
+		temp = false;
+	}
+
+	return temp;
+}
+public double theAnswer()
+{
+	String digits;
+	double dNum;
+	double dNumFinal=0;
+	for (int n = 2; n<e.length()-10; n++)
+	{
+		digits = e.substring(n,n+10);
+		dNum = Double.parseDouble(digits); 
+		if (isPrime(dNum))
+			dNumFinal=dNum;
+		
+	}
+	return dNumFinal;
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
     if (passedArgs != null) {
